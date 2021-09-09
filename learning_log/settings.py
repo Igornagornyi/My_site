2c3e1ef40a22cda1097750e5e67191495f38986d
Igor Nagornyi <nagornyi.dnepr@gmail.com>
2021-09-09 08:53:03 +0300

    debug true

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i!k@e9yj4w)0&!-8i6x&i$wj=)jebw()8utoigk46l2x5p%h(%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -132,3 +132,7 @@ import os
 import django_heroku
 django_heroku.settings(locals())
 
+if os.environ.get('DEBUG') == 'True':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'False':
+    DEBUG = False
